b4fa16e5854e0f18141e8deb27415be660b056b1
Ben Megidish <megidish46@gmail.com>
2023-02-06 23:52:27 +0200

    third commit

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const router=require('./routers/router');
 app.use(cors());
 app.use(express.json());
 app.use('/api/router',router);
+app.use('/',(req,res)=>{
+    res.send("Hello first time using railways");
+})
 const port=4000;
 app.listen(port,()=>{
     console.log(`Server is Runung on port ${port}`);
