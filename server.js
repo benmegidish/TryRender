0f39fc70dbe5bb08e49e55f440dd2ca6d75b353d
Ben Megidish <megidish46@gmail.com>
2023-02-07 03:41:21 +0200

    changed server

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const server =http.createServer(app);
 
 const io=new Server(server,{
     cors:{
-        origin:"https://socketio-trial.netlify.app/",
+        origin:"https://second-socketio-trial.netlify.app/",
         methods:["GET","POST"]
     }      
 })
